3622bb06bddae95bdc1b2554b6522fa7dead0b12
Simone Taeggi <simone.taeggi@leovegas.com>
2018-06-08 10:14:58 +0200

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
 /*
 This exercise will help you practice many of your newly aquired React skills.
 
@@ -28,10 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
+        <Header logo={logo} />
         <div className="container">
           <div className="chat-window">
             <h2>Super Awesome Chat</h2>
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = (props) => (
+    <header className="App-header">
+        <img src={props.logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">ReactND - Coding Practice</h1>
+    </header>
+);
+
+Header.propTypes = {
+    logo: PropTypes.string.isRequired
+};
+
+export default Header;
